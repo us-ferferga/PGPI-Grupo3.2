c4380fe909cd7506c9c9ceeb9ad7f4ab76e08b19
Fernando Fernández <ferferga@hotmail.com>
2023-12-02 20:11:44 +0100

    refactor(frontend): lint and typecheck
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

--- frontend/src/components/View/clase/carrito.vue
+++ frontend/src/components/View/clase/carrito.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-btn square color="purple" glossy>
-      <i-mdi-cart></i-mdi-cart>
-    </q-btn>
+    <QBtn
+      square
+      color="purple"
+      glossy>
+      <IMdiCart />
+    </QBtn>
   </div>
 </template>
--- frontend/src/components/View/clase/horario.vue
+++ frontend/src/components/View/clase/horario.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="contenedor">
-    <h2 style="display: flex;align-items: center;  justify-content: center;">Horario</h2>
-    <Tabla class="tabla"></Tabla>
+    <h2 style="display: flex;align-items: center;  justify-content: center;">
+      Horario
+    </h2>
+    <Tabla class="tabla" />
   </div>
 </template>
 
-<script setup lang="ts">  
+<script setup lang="ts">
 
 
 </script>
 
-<style>
+<style scoped>
   .contenedor{
     background-color: #f6f5f5;
     margin-top: 5%;
-  }
-
-  .tabla{
-   
   }
 </style>
