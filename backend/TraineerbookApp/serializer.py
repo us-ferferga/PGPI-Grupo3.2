6d1a48d736ef255edcc0ad80b76544415c9cb337
Juan Carlos GR <juacargomram@gmail.com>
2023-12-03 20:16:02 +0100

    feat (Product): Added Product list and getter
    Signed-off-by: Juan Carlos GR <juacargomram@gmail.com>

---
+++ backend/TraineerbookApp/serializer.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from TraineerbookApp.models import *
+from rest_framework.serializers import ModelSerializer
+from TraineerbookApp.serializer import *
+
+class ActivitySerializer(ModelSerializer):
+    class Meta:
+        model = Activity
+        fields = '__all__'
+
+class TeacherSerializer(ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+class ClassRoomSerializer(ModelSerializer):
+    class Meta:
+        model = ClassRoom
+        fields = '__all__'
+
+class GetProductSerializer(ModelSerializer):
+    activity = ActivitySerializer()
+    teacher = TeacherSerializer(source='activity.teacher')
+    class_space = ClassRoomSerializer(source='activity.class_space')
+
+    class Meta:
+        model = Product
+        fields = '__all__'
