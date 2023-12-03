2dd09e6468738191d7623c16d8dbb24c8c995476
carlosdelrioperez <80700839+carlosdelrioperez@users.noreply.github.com>
2023-12-03 12:55:13 +0100

    feat(frontend): company data
    Signed-off-by: carlosdelrioperez <80700839+carlosdelrioperez@users.noreply.github.com>

---
+++ frontend/src/pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contenedor">
+    <Texto />
+  </div>
+  <ImagenDatosEmpresa />
+  <FooterDatosEmpresa />
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+  .contenedor{
+    text-align: center;
+    width: 1000px;
+    margin-left: 250px;
+  }
+
+</style>
