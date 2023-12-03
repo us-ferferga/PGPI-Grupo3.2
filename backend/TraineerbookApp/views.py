e2b1d6b9e1eaf73aef96163ebfbd4c6d13fb5602
Juan Carlos GR <juacargomram@gmail.com>
2023-12-03 20:16:02 +0100

    feat (Product): Added Product list and getter
    Signed-off-by: Juan Carlos GR <juacargomram@gmail.com>

--- backend/TraineerbookApp/views.py
+++ backend/TraineerbookApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from requests import Response
+from TraineerbookApp.models import *
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from TraineerbookApp.serializer import *
 
 # Create your views here.
+
+
+class getProductsApiViewSet(ModelViewSet):
+  http_method_names = ['get']
+  
+  def get_queryset(self):
+    products = Product.objects.all()
+    serializer = GetProductSerializer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class getProductDetailApiViewSet(ModelViewSet):
+  http_method_names = ['get']
+
+  def get_queryset(self,request,pk):
+    product = Product.objects.get(id=pk)
+    serializer = GetProductSerializer(product, many=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
