e8f55f8d4b5cca88e1a702bb90bcf08ce5e36738
Fernando Fernández <ferferga@hotmail.com>
2023-12-03 23:00:08 +0100

    feat(frontend): create user avatar component
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

---
+++ frontend/src/components/layout/UserAvatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <QAvatar
+    color="blue"
+    class="cursor-pointer">
+    <IFluentPerson28Filled />
+    <QMenu
+      transition-show="jump-down"
+      transition-hide="jump-up">
+      <QList style="min-width: 100px">
+        <RouterLink
+          v-slot="{ navigate }"
+          to="/orders"
+          custom>
+          <QItem
+            clickable
+            @click="navigate()">
+            <QItemSection>Mis pedidos</QItemSection>
+          </QItem>
+        </RouterLink>
+        <QSeparator />
+        <QItem clickable>
+          <QItemSection>Cerrar sesión</QItemSection>
+        </QItem>
+      </QList>
+    </QMenu>
+  </QAvatar>
+</template>
+
+<script setup lang="ts">
+</script>
