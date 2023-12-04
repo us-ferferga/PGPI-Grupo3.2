2efc8121a3ba0bbeda72f38a3e5206505d7509c0
Fernando Fernández <ferferga@hotmail.com>
2023-12-04 18:27:29 +0000

    refactor(backend): move database into subdirectory
    In order to have a working Docker image, the database must be persisted.
    In ocker, folders are mounted from the container to the host, so we should mount
    a folder that contains the only data needed to be persisted, not the whole application
    
    Signed-off-by: GitHub <noreply@github.com>

--- backend/traineerbook/settings.py
+++ backend/traineerbook/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATABASE_FOLDER = BASE_DIR / 'data'
+# Create database folder if it doesn't exist
+Path.mkdir(DATABASE_FOLDER, exist_ok=True)
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,14 +81,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'traineerbook.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'TraineerBook.db',
+        'NAME': DATABASE_FOLDER / 'TraineerBook_data.db',
     }
 }
 
