c69431e46f5c17089b961b7e7719eba18a9d5538
Fernando Fernández <ferferga@hotmail.com>
2023-12-11 00:30:29 +0000

    refactor(backend): remove unused dependency, test CORS settings
    rest_framework_swagger is deprecated and we're using drf_yasg
    instead. Removing it since it can break Django apps.
    
    * Allow all origins for CORS compliance in browsers
    
    Signed-off-by: GitHub <noreply@github.com>

--- backend/traineerbook/urls.py
+++ backend/traineerbook/urls.py
@@ -19,7 +19,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rest_framework_swagger.views import get_swagger_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
