2b474a066976ae293bd9e3e6946edf1b0fef56f6
carlosdelrioperez <80700839+carlosdelrioperez@users.noreply.github.com>
2023-12-12 18:12:37 +0100

    feat: enlazado con backend el nombre, profesor y sala, la imagen todavía no funciona, falta descripcion, horario, comentarios y los botones de compra
    Signed-off-by: carlosdelrioperez <80700839+carlosdelrioperez@users.noreply.github.com>

--- frontend/src/composables/use-activities.ts
+++ frontend/src/composables/use-activities.ts
@@ -8,7 +8,7 @@ export const activities = ref <Activity[]>([]);
  */
 export async function useActivities():Promise<Ref<Activity[]>> {
   const server = useServer();
-  const activityResponse = await server.activity.activityList();
+  const activityResponse = await server.api.activity.activityList();
 
   activities.value = activityResponse.data;
 
