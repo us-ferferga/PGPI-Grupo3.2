f1ad4966f624bf4e7171fe508095da4f14ad17c8
Fernando Fernández <ferferga@hotmail.com>
2023-12-13 18:28:22 +0000

    fix(backend): path for templates
    Signed-off-by: GitHub <noreply@github.com>

--- backend/traineerbook/settings.py
+++ backend/traineerbook/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
@@ -99,7 +98,7 @@ ROOT_URLCONF = 'traineerbook.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'TraineerbookApp', 'templates', 'email')],
+        'DIRS': [BASE_DIR / 'TraineerbookApp' / 'templates' / 'email'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -154,16 +153,6 @@ TIME_ZONE = 'Europe/Madrid'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
-MEDIA_URL = None
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
